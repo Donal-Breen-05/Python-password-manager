18028ccd643d0025282e7fba1ff18c0f15596b05
Donal-Breen-05 <donalbreen50@gmail.com>
2025-10-24 13:44:34 +0100

    main.py
    add User function 
    + formating

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import json
 import hashlib 
 import os, sys 
 
+""" 
+    SECURITY 
+the majority of the encryption     
+""" 
+
 #hashing 
 def masterPasswordHash(masterPassword): 
     hash = haslib.sha256()
     sha256.update(masterPassword.encode())
     return sha256.hexdigest 
-
-""" 
-    SECURITY 
-the majority of the encryption     
-""" 
 
 
 #encryption key 
@@ -28,5 +28,25 @@ def initCipher(key):
 def enryptPassword(cipher, password): 
     return cipher.encrypt(password.encode()).decode()
 
-    # 
+
+""" 
+    Users and User data 
+the functions that take care of user data 
+"""
+
+def newUser(username, masterPassword): 
+    hash = masterPasswordHash(masterPassword) 
+    
+    userData = {'UserName': username , 'MasterPassword': hash }
+    fileName = 'data.json' 
+
+    if os.exists(fileName): 
+        with open(fileName , "a") as file: 
+            json.dump(user_data, file)
+            print("\nUser added\n")
+    else: 
+        with open(fileName , "x") as file: 
+            json.dump(userData, file) 
+            print("user added") 
+
 
