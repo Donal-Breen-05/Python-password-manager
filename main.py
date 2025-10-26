c70fc21989bdf2df289c8a6ed292fba5a3eb7dd5
Donal-Breen-05 <donalbreen50@gmail.com>
2025-10-26 08:48:44 +0000

    general fixes + added login and view passwords

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import json
 import hashlib 
 import os, sys 
 
+
+
 """ 
     SECURITY 
 the majority of the encryption     
 """ 
 
 #hashing 
-def masterPasswordHash(masterPassword): 
+def hashPassword(password): 
     hash = haslib.sha256()
     sha256.update(masterPassword.encode())
     return sha256.hexdigest 
@@ -48,5 +50,55 @@ def newUser(username, masterPassword):
         with open(fileName , "x") as file: 
             json.dump(userData, file) 
             print("user added") 
+    encryptedPassword = encryptPassword(cipher)
+
+#add 
+def addUserPass(password, site): 
+
+    try: 
+        with open("data.json" , "w") as file: 
+            data = json.load(file)
+    except json.JSONDecodeError:
+        #error check for empty or broken json 
+        data = [] 
+
+    #encrypt passwords 
+
+#login 
+def login(userName, password):
+    try: 
+        with open("data.json") as file: 
+            userData = json.load(file) 
+        
+        #check user entered password with master password
+        masterPasswordHash = userData.get("masterPassword")
+        passwordAttemptHash = hashPassword(passwordAttemptHash)
+        
+        if passwordAttemptHash == masterPasswordHash and userName == userData.get("userName"):
+            print("\nlogin successful")
+        else: 
+            print("\nlogin unsuccessful")
+            #exit
+            sys.exit()
+    except Exception: 
+        print("\nYoure not registered") 
+        #exit
+        sys.exit()
+
+#view passwords
+def viewPasswords(): 
+    try: 
+        with open("data.json", "r") as data:
+            view = json.load(data)
+            print("\nyour passwords")
+
+            for password in view: 
+                print(x["website"])
+            print("\n")
+    except FileNotFoundError: 
+        print("\nYou havent saved any passwords")
+
+
+
 
 
